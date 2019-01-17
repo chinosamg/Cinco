1931c07a8f3c19cef831baa178361c740b90b8d6
Samuel Guzman <[chinosamg@gmail.com]>
2019-01-16 19:23:25 -0700

    update to proposal

--- Project Proposal.py
+++ Project Proposal.py
@@ -11,22 +11,33 @@
 #       o	San Diego, CA
 
 #   * The kinds of questions you'll be asking of that data
-#  • Current market conditions – ability to sell
-#       o	ROI
-#       o	Price per sq ft.
-#       o	Days on the market
-#  • Vacation rental conditions
-#       o	Saturation of market
-#       o	Price per night/avg
 
-#    * Possible source for such data
+#   • Current market conditions – ability to sell
+#       o	ROI
+#       o	Purchase price per sq ft.
+#       o	Equity change for ownership timeframe 
+#       o	Long term: Days on the market/avg
+#       o	Short term: Number of night available/avg
+
+#   • Vacation rental conditions
+#       o	Saturation of market
+#       o	Short term rentals: Price per night/avg
+#       o	Long term rentals: Price per month/avg
+
+#   • Possible source for such data
 #       o	Inside Airbnb
 #       o	Realtor.com
+#       o	Zillow.com
 
-#   In other words, write down what kind of data you plan to work with, and what kinds of questions you'd like to ask of it. 
-#   This constitutes your Project Proposal/Outline, and should look something like this:
-
-#   An anonymous benefactor has a $5,000,000 budget and hired a consulting firm to come up with recommendations for the best places to purchase real estate.  
-#   The firm must analyze at least five markets in the United States to determine if the regions are lucrative investment options.  
-#   Some factors the firm will consider when determining which areas to recommend are: ROI, current market conditions, and vacation rental conditions. 
+#       *   Background:
+#               An anonymous benefactor has a $5,000,000 budget and hired a consulting firm to 
+#               come up with recommendations for the best places to purchase real estate.  
+#               The firm must analyze at least five markets in the United States to determine 
+#               if the regions are lucrative investment options.  
+#               Some factors the firm will consider when determining which areas to recommend are: 
+#               ROI, current market conditions, and vacation rental conditions. 
+#
+#       *   Hypothesis:
+#               Rental income in a specific state is determined by type of occupancy short term versus long term. 
+#               Some factors to condider will be vacancy, days on the market and acqusition cost.
 
